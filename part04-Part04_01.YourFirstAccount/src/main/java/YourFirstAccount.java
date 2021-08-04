a97f43f0f5952d86896aa6e61d66c2bf4152e12c
Neil Ryan <neilryanlipaod@gmail.com>
2021-08-04 09:54:01 +0800

    CHECKPOINT PART04_07.Product

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,11 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
-        Account matthews = new Account("Matthews account", 1000.0);
-        Account mine = new Account("My account", 0.0);
-        matthews.withdrawal(100.0);
-        mine.deposit(100.0);
-        System.out.println(matthews.toString());
-        System.out.println(mine.toString());
+       Account nil = new Account("Nil", 100.0);
+       nil.deposit(20.0);
+       System.out.println(nil.toString());
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account matthews = new Account("Matthews account", 1000.0);
+        Account mine = new Account("My account", 0.0);
+        matthews.withdrawal(100.0);
+        mine.deposit(100.0);
+        System.out.println(matthews.toString());
+        System.out.println(mine.toString());
     }
 }
