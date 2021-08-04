8e000b1ff60a8c73121dbddcaf04789976f88b8d
Neil Ryan <neilryanlipaod@gmail.com>
2021-08-04 18:25:26 +0800

    HALFWAY through Part04
    Finished Introduction to OOP whic is up to Part04_16.

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,6 +5,10 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Statistics all = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+     
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
@@ -12,5 +16,25 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers");
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == -1) {
+                break;
+            }
+            if(input % 2 == 0) {
+                even.addNumber(input);
+            } else {
+                odd.addNumber(input);
+            }
+            
+            all.addNumber(input);
+        }
+        
+        System.out.println("Sum: " + all.sum());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of odd numbers: " + odd.sum());
+                
     }
 }
